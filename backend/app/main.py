1d5391308ae9dc3e2a48a62ad6b0c3ee1053f964
matheusrfm23 <227707883+matheusrfm23@users.noreply.github.com>
2025-08-29 22:32:08 -0300

    Update main.py
    Geoprumo

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 origins = [
     "http://localhost:3000",
     "http://192.168.0.86:3000",
-    https://geoprumo-app.onrender.com# <-- NOVO ENDEREÇO DE REDE ADICIONADO
+    "https://geoprumo-app.onrender.com",# <-- NOVO ENDEREÇO DE REDE ADICIONADO
     # Adicione aqui outros endereços se necessário (ex: o futuro endereço de produção na Render)
 ]
 
